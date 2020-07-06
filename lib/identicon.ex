9a5219bdedf133539c9e5f952119104d064f2dc4
José Victor Zeller <josevictorzg@gmail.com>
2020-07-05 22:21:05 -0300

    feat: Create the Identicon repository with Image struc and some functions

---
+++ lib/identicon.ex
@@ -0,0 +1,25 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  alias Identicon.Image
+
+  def start(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+  end
+
+  def pick_color(%Image{hex: [r, g, b | _tail]} = image) do
+    %Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+      %Image{hex: hex}
+  end
+end
